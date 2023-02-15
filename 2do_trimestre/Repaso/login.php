440b21f0b9ad116f7398fb5e94bbebbed222f5a7
Pablo Gallego <70765766+Pablo-Gallego@users.noreply.github.com>
2023-02-15 13:40:35 +0100

    falta la tabla

---
+++ 2do_trimestre/Repaso/login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Consulta</title>
+</head>
+<body align="center">
+<?php
+error_reporting(0)
+?>
+   <h1 style="text-decoration: underline;">Login</h1>
+   <form method='POST' action='login.php'>
+       nombre: <input type='text' name='nombre'> <br><br>
+       <input type='submit' name='Consultar' value='Consultar'>
+       </form>
+       <br>
+
+<?php
+if (isset($_POST["Consultar"])) 
+{
+    $nombre = $_POST["nombre"];
+
+    $idConexion = mysqli_connect( "localhost", "root" , "" );
+    if (!$idConexion) 
+    {
+         die('No se puede conectar' );
+    }
+
+    $seleccionada = mysqli_select_db($idConexion,"login");
+    if (!$seleccionada) 
+    {
+        die ("No se puede usar la base de datos: ". mysqli_error($idConexion)); 
+    }
+    
+    $consulta = "SELECT nombre, apellido1, apellido2, fechanacimiento, email FROM usuarios WHERE nombre LIKE %$nombre%";
+    $resultado = mysqli_query($idConexion, $consulta);
+
+    if (mysqli_num_rows($resultado) > 0) 
+    {
+    $row = mysqli_fetch_assoc($resultado);
+    $nombre = $row["nombre"];
+    $apellido1 = $row["apellido1"];
+    $apellido2 = $row["apellido2"];
+    $fechanacimiento = $row["fechanacimiento"];
+
+    echo "<table>
+    
+    </table>";
+    } else 
+    {
+        echo "<br><br>No estás registrado o la contraseña es incorrecta.";
+    }
+    mysqli_close($idConexion);
+} else 
+{
+   echo "<br><br>Introduce tus datos";
+}
+    mysqli_close($idConexion);
+?>
+</body>
+</html>
