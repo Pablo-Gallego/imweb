0d5aaf1b82794e177016f9524023eb57c8ab2bf8
Pablo Gallego <70765766+Pablo-Gallego@users.noreply.github.com>
2023-02-22 08:31:18 +0100

    formateo

--- 2do_trimestre/alberto/mismodocumento.php
+++ 2do_trimestre/alberto/mismodocumento.php
@@ -1,16 +1,17 @@
 <?php
-function hola() {
+function hola()
+{
     global $idConexion;
     $idConexion = mysqli_connect("localhost", "root", "");
     if (!$idConexion) {
         die("Error al conectar");
     }
-    $seleccionada = mysqli_select_db($idConexion, "login"); 
+    $seleccionada = mysqli_select_db($idConexion, "login");
     if (!$seleccionada) {
         die("Error al buscar");
-    } 
+    }
     return $idConexion;
-} 
+}
 ?>
 <form method="POST">
     Nombre<input type="text" name="letra"><br>
@@ -19,7 +20,7 @@ function hola() {
 </form>
 <?php
 if (isset($_POST['Enviar'])) {
-    
+
 
     mysqli_free_result($resultado);
     mysqli_close($idConexion);
